1a4e643eca2773122c780ec2d1b2b05fee077fee
shahed <shahed.rahman88@gmail.com>
2022-03-18 21:30:50 +0600

    integrated https module and api key

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const https = require('https');
 
 const app = express();
 
 app.get("/", (req, res)=> {
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=527f01b50d782f5c4801ebc04130f077";
+    https.get(url, (response)=> {
+        console.log(response);
+    })
+
     res.send("Welcome")
 })
 
